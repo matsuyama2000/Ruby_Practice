b242707582bff46754937e43e18de283bf2a654f
Emily Ko <emily20000222@keio.jp>
2023-04-22 00:42:25 +0800

    refactor code to use chars instead of split, and remove unnecessary map call in join statement for improved conciseness

--- candy/candy005.rb
+++ candy/candy005.rb
@@ -4,8 +4,10 @@
 
 def square_digits(num)
   # 實作寫在這裡
-  # 3212 => '3212' => ['3', '2', '1', '2'] => [3, 2, 1, 2] => [9, 4, 1, 4] => '9414' => 9414
-  return num.to_s.split("").map { |x| x.to_i }.map { |x| x * x }.join('').to_i
+  # 3212 => '3212' => ['3', '2', '1', '2'] => [9, 4, 1, 4] => '9414' => 9414
+  # chars returns an array of characters that make up the string
+  # when join is called without an argument, it assumes that the separator should be an empty string
+  return num.to_s.chars.map { |x| x.to_i ** 2 }.join.to_i
 end
 
 p square_digits(3212) # 印出 9414
