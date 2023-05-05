8fca2d381262301ad53e0a5b1232dcfea0c94b31
Emily Ko <emily20000222@keio.jp>
2023-05-05 16:36:18 +0800

    fix candy007. result should not be an array

--- candy/candy007.rb
+++ candy/candy007.rb
@@ -3,18 +3,20 @@
 # 題目：在某個數字陣列裡，可能藏有某個不合群的奇數或偶數，試著找出它！
 
 def find_some_different(numbers)
-  # 實作寫在這裡
-  # first_array, second_array = array.partition { |element| condition }
-  odd_array, even_array = numbers.partition { |number| number % 2 != 0 }
+  odd_count = numbers.count { |num| num.odd? }
+  even_count = numbers.count { |num| num.even? }
 
-  # compare the size of the odd and even arrays, and retrieve the smaller one
-  if odd_array.size > even_array.size
-    return even_array
+  # if the count of odd numbers is greater than the count of even numbers,
+  # then the majority of numbers are odd, so return the first even number.
+  if odd_count > even_count
+    return numbers.find { |num| num.even? }
+  elsif even_count > odd_count
+    return numbers.find { |num| num.odd? }
   else
-    return odd_array
+    return "no number with different parity found!"
   end
-
 end
+
 
 puts find_some_different([2, 4, 0, 100, 4, 11, 2602, 36]) # 印出 11
 puts find_some_different([160, 3, 1719, 19, 11, 13, -21]) # 印出 160
