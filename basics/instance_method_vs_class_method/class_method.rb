3c844802985f787362ad745c392421df42c40127
Emily Ko <emily20000222@keio.jp>
2023-06-10 17:48:46 +0800

    add instance method & class method notes

---
+++ basics/instance_method_vs_class_method/class_method.rb
@@ -0,0 +1,7 @@
+class Cat
+  def self.all # 方法前面加上 self
+    puts "全部的貓兒"
+  end
+end
+
+Cat.all # 類別方法 = 作用在類別上方法
---
+++ basics/instance_method_vs_class_method/instance_method.rb
@@ -0,0 +1,8 @@
+class Cat
+  def say_hello
+    puts "你好"
+  end
+end
+
+kitty = Cat.new
+kitty.say_hello # 實體方法 = 作用在實體上方法
