b3c613ebdb6ddc88701b887d8095e6be81123f86
Emily Ko <emily20000222@keio.jp>
2024-03-01 02:39:43 +0900

    fix codewar level 6 rspec issue

--- codewar/level6/001.rb
+++ codewar/level6/001.rb
@@ -10,7 +10,11 @@ def to_camel_case(str)
   str.split(/-|_/).map.with_index { |word, index| index.zero? ? word : word.capitalize }.join
 end
 
-Test.assert_equals(to_camel_case(''), '', "An empty string was provided but not returned")
-Test.assert_equals(to_camel_case("the_stealth_warrior"), "theStealthWarrior", "to_camel_case('the_stealth_warrior') did not return correct value")
-Test.assert_equals(to_camel_case("The-Stealth-Warrior"), "TheStealthWarrior", "to_camel_case('The-Stealth-Warrior') did not return correct value")
-Test.assert_equals(to_camel_case("A-B-C"), "ABC", "to_camel_case('A-B-C') did not return correct value")
+describe "to_camel_case function" do
+  it "Fixed Tests" do
+    expect(to_camel_case('')).to eq ''
+    expect(to_camel_case('the_stealth_warrior')).to eq 'theStealthWarrior'
+    expect(to_camel_case('The-Stealth-Warrior')).to eq 'TheStealthWarrior'
+    expect(to_camel_case('A-B-C')).to eq 'ABC'
+  end
+end
